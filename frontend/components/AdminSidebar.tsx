75450a1472e7a913608dae0c9aa971a24c93b29b
Bil <bildadsimiyu6@gmail.com>
2024-10-18 16:43:44 +0300

    add sidebar links

--- frontend/components/AdminSidebar.tsx
+++ frontend/components/AdminSidebar.tsx
@@ -2,17 +2,23 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
-import { Home, TrendingDown, TrendingUp } from "react-feather";
+import { Home, Speaker, TrendingUp } from "react-feather";
+import { BsArrowBarLeft } from "react-icons/bs";
+import { FaUserGraduate } from "react-icons/fa";
+import {
+  FaUserTie,
+  FaClipboardUser,
+  FaBookOpen,
+  FaListCheck,
+  FaUser,
+} from "react-icons/fa6";
 import {
   MdArrowBack,
   MdMenu,
-  MdEmojiPeople,
-  MdFilePresent,
-  MdBook,
   MdCalendarMonth,
-  MdChat,
-  MdQuestionAnswer,
+  MdNotifications,
 } from "react-icons/md";
+import { SiBookstack } from "react-icons/si";
 import OutsideClickHandler from "react-outside-click-handler";
 
 export default function AdminSidebar() {
@@ -28,18 +34,110 @@ export default function AdminSidebar() {
         <div
           className={`sidebar relative max-w-[300px] ${sidebarOpen ? "sidebar-open" : ""}`}
         >
+          <button className="absolute top-3 left-2 gap-1 hover:text-white cursor-pointer text-gray flex items-center border p-1 rounded-md"><BsArrowBarLeft/>Logout</button>
           <MdArrowBack
             className="absolute top-3 right-2 size-6 cursor-pointer hover:w-10 hover:h-8 hover:text-gray 768px:hidden"
             onClick={() => setSidebarOpen(false)}
             tabIndex={1}
           />
-          <ul className="768px:pt-5 max-768px:pt-[60px] px-2">
+          <ul className="768px:pt-5 max-768px:pt-[60px] px-2 flex flex-col gap-5 pb-5">
             <li>
               <Link
                 href={"/admin"}
                 className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin" ? "bg-purple" : ""}`}
               >
                 <Home className="size-5" /> <span>Admin Dashboard</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href={"/admin/students"}
+                className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin/students" ? "bg-purple" : ""}`}
+              >
+                <FaUserGraduate className="size-5" /> <span>Students</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href={"/admin/teachers"}
+                className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin/teachers" ? "bg-purple" : ""}`}
+              >
+                <FaUserTie className="size-5" /> <span>Teachers</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href={"/admin/assignments"}
+                className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin/assignments" ? "bg-purple" : ""}`}
+              >
+                <FaClipboardUser className="size-5" /> <span>Assignments</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href={"/admin/exams"}
+                className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin/exams" ? "bg-purple" : ""}`}
+              >
+                <FaBookOpen className="size-5" /> <span>Exams</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href={"/admin/performance"}
+                className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin/performance" ? "bg-purple" : ""}`}
+              >
+                <TrendingUp className="size-5" /> <span>Performance </span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href={"/admin/attendance"}
+                className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin/attendance" ? "bg-purple" : ""}`}
+              >
+                <FaListCheck className="size-5" /> <span>Attendance</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href={"/admin/event-calender"}
+                className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin/event-calender" ? "bg-purple" : ""}`}
+              >
+                <MdCalendarMonth className="size-5" />{" "}
+                <span>Event Calender</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href={"/admin/library"}
+                className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin/library" ? "bg-purple" : ""}`}
+              >
+                <SiBookstack className="size-5" /> <span>Library</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href={"/admin/announcements"}
+                className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin/announcements" ? "bg-purple" : ""}`}
+              >
+                <MdNotifications className="size-5" /> <span>Announcements</span>
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                href={"/admin/profile"}
+                className={`flex items-center py-1 rounded-lg px-1 gap-2 hover:text-gray ${pathname === "/admin/profile" ? "bg-purple" : ""}`}
+              >
+                <FaUser className="size-5" /> <span>Settings &amp; Profile</span>
               </Link>
             </li>
           </ul>
