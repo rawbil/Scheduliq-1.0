9ca8bbc9a6aa28143b7be92570475c654480b43c
Bil <bildadsimiyu6@gmail.com>
2024-10-20 08:32:23 +0300

    update admin/exams route

--- frontend/app/admin/exams/page.tsx
+++ frontend/app/admin/exams/page.tsx
@@ -1,7 +1,55 @@
-
-
 export default function Exams() {
-    return (
-        <div>Exams</div>
-    )
+
+  return (
+    <div className="max-768px:mt-10">
+      <h2 className="w-fit mx-auto border-b mb-10 text-xl">Exam Details</h2>
+      <form>
+        <label htmlFor="examName" className="text-gray">
+          Name:{" "}
+        </label>
+        <input
+          type="text"
+          name="examName"
+          id="examName"
+          placeholder="Enter Exam Name"
+          className="w-full p-[8px] mb-5 rounded-[4px] text-black border-[1px] border-red"
+        />
+
+        <label htmlFor="examClass" className="text-gray">
+          Class:{" "}
+        </label>
+        <input
+          type="text"
+          name="examClass"
+          id="examClass"
+          placeholder="Enter Exam Class"
+          className="w-full p-[8px] mb-5 rounded-[4px] text-black border-[1px] border-red"
+        />
+
+        <label htmlFor="examMarks" className="text-gray">
+          Marks:{" "}
+        </label>
+        <input
+          type="text"
+          name="examMarks"
+          id="examMarks"
+          placeholder="Enter Exam Marks"
+          className="w-full p-[8px] mb-5 rounded-[4px] text-black border-[1px] border-red"
+        />
+
+        <button
+          type="submit"
+          className="mx-auto mb-5 grid bg-purple p-1.5 rounded-[4px]"
+        >
+          Submit
+        </button>
+
+        <h2>Total Marks: </h2>
+        <h2>Exam Details: </h2>
+        <ul>
+
+        </ul>
+      </form>
+    </div>
+  );
 }
