9cf531cca47af330afb84220e032695ad5fe9b67
Bil <bildadsimiyu6@gmail.com>
2024-10-22 12:51:41 +0300

    update teachers layout, assignments

--- frontend/app/teachers/assignments/page.tsx
+++ frontend/app/teachers/assignments/page.tsx
@@ -2,6 +2,26 @@
 
 export default function Assignments() {
     return (
-        <div>Assignments</div>
+        <div className="max-768px:mt-10">
+            <h2 className="text-center text-xl font-serif font-semibold mb-5">Assignments</h2>
+            <form className="max-w-[600px] w-full mx-auto">
+                <input type="text" name="assignmentTitle" id="assignmentTitle" placeholder="Enter Assignment Title" className="w-full border-[1.5px] border-purple rounded p-2 mb-5 text-black" />
+
+                <textarea name="assignmentDescription" id="assignmentDescription" className="w-full p-2 border-[1.5px] border-purple rounded text-black mb-5" rows={4} placeholder="Enter Assignment Description"></textarea>
+
+                <input type="text" name="assignmentGrade" id="assignmentGrade" placeholder="Enter Assignment Grade" className="w-full border-[1.5px] border-purple rounded p-2 mb-5 text-black" />
+
+                <input type="date" name="assignmentDeadline" id="assignmentDeadline" placeholder="Enter Assignment Deadline" className="w-full border-[1.5px] border-purple rounded p-2 mb-5 text-black" />
+
+                <button type="submit" className="bg-purple mt-5 rounded p-1.5 hover:bg-purple/90">Submit Assignment</button>
+            </form>
+            <hr className="mt-10" />
+
+            {/* Assignment List */}
+            <div className="mt-10">
+                <h2 className="text-center font-serif font-semibold text-xl">Assignment List</h2>
+                {/* add assignment list here */}
+            </div>
+        </div>
     )
 }
--- frontend/app/teachers/layout.tsx
+++ frontend/app/teachers/layout.tsx
@@ -3,7 +3,7 @@ import TeacherSidebar from "@/components/TeacherSidebar";
 
 export default function TeacherLayout({children}: Readonly<{children: React.ReactNode}>) {
     return (
-    <div>
+    <div className="flex gap-1">
         <TeacherSidebar />
         <main className="admin-children 768px:pt-20 px-4 max-768px:mt-10">{children}</main> 
     </div>
