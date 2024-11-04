f4f7b628209c4f9089374b1f2c2a27b0ee283f37
Bil <bildadsimiyu6@gmail.com>
2024-11-04 16:29:28 +0300

    update events model and schema

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-
+    event: {
+        type: String,
+        required: true
+    }
 });
 
 const eventModel = mongoose.model('Event', eventSchema);
--- backend/routes/eventRoute.js
+++ backend/routes/eventRoute.js
@@ -2,4 +2,34 @@ const express = require('express');
 const route = express.Router();
 const eventModel = require('../models/eventModel');
 
+//POST /events/create
+route.post("/create", async(req, res) => {
+    try {
+        const {event} = req.body;
+        if(!event) {
+            return res.json({success: false, message: "Please fill in the required fields"});
+        }
+
+        const newEvent = await eventModel.create({event});
+        res.status(200).json({success: true, message: "Event added successfully", newEvent});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: "Error creating events"});
+    }
+})
+
+//GET events/get
+route.get('/get', async(req, res) => {
+    try {
+        const events = await eventModel.find();
+        if(!events) {
+            return res.json({message: "Event list empty"});
+        }
+        res.status(200).json({success: true, message: "Events fetched successfully", events});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error getting events"})
+    }
+})
+
 module.exports = route;
